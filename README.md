20817ed53c880e3f49d6afef3406215d599e2129
田杰 <jefftian@hcdlearning.com>
2016-05-10 18:03:23 +0800

    healthcheck.

---
+++ README.md
@@ -0,0 +1,4 @@
+## Run on your machine
+```
+npm start
+```
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
+});
+
+router.get('/healthcheck', function (req, res, next) {
+    res.json({
+        everything: 'is ok',
+        time: new Date()
+    });
 });
 
 module.exports = router;
