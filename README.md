fe69f8642121ebfc7327f5255e96296069c9b564
田杰 <jefftian@hcdlearning.com>
2016-05-24 13:01:36 +0800

    Use webcomponent to integrate footer.

--- README.md
+++ README.md
@@ -5,3 +5,10 @@ bower install
 
 npm start
 ```
+
+# Generate html files from jade
+If you changed jade file, you should run 
+```
+gulp jade
+```
+to generate html files accordingly.
---
+++ gulpfile.js
@@ -0,0 +1,65 @@
+var
+    gulp = require('gulp'),
+    print = require('gulp-print'),
+    runSequence = require('run-sequence'),
+    uglify = require('gulp-uglify'),
+    clean = require('gulp-clean'),
+    uglifyCss = require('gulp-minify-css'),
+    jade = require('gulp-jade')
+    ;
+
+gulp.task('build', function (done) {
+    runSequence('clean', 'jade', 'copy', 'uglify-js', 'uglify-css', done);
+});
+gulp.task('default', ['build']);
+
+gulp.task('clean', function (done) {
+    return gulp.src('dist', {read: false})
+        .pipe(clean())
+        ;
+});
+
+gulp.task('copy', function (done) {
+    return gulp.src(['public/**/*'])
+        .pipe(gulp.dest('dist/'))
+        ;
+});
+
+gulp.task('uglify-js', function (done) {
+    return gulp.src('public/scripts/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/scripts'))
+        ;
+});
+
+gulp.task('uglify-css', function (done) {
+    return gulp.src('public/stylesheets/*.css')
+        .pipe(uglifyCss())
+        .pipe(gulp.dest('dist/stylesheets'))
+        ;
+});
+
+gulp.task('release', function (done) {
+    // Do nothing
+    done();
+});
+
+gulp.task('jade', function (done) {
+    var jadeFiles = [{
+        src: './views/index.jade',
+        dest: './public/'
+    }];
+
+    return jadeFiles.forEach(function (jf) {
+        if (!jf.src || !jf.dest) return;
+
+        gulp.src(jf.src)
+            .pipe(jade())
+            .pipe(gulp.dest(jf.dest))
+        ;
+    });
+});
+
+module.exports = function (callback) {
+    runSequence('build', callback);
+};
