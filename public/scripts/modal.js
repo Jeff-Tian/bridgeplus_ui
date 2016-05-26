d211258be28c3d5350ad59dda78dbbbc812a223a
田杰 <jefftian@hcdlearning.com>
2016-05-26 14:00:30 +0800

    Delete  method.

--- public/scripts/modal.js
+++ public/scripts/modal.js
@@ -1,17 +1,17 @@
 $(document).ready(function () {
     function prepare() {
-        $get('.mask .text').each(function () {
+        $('.mask .text').each(function () {
             var $this = $(this);
             var $image = $this.next('.image');
             $this.width($image.width() + 'px');
             $this.css('min-height', $image.height() + 'px');
         });
 
-        $get('.mask .close.button').click(function () {
+        $('.mask .close.button').click(function () {
             $(this).closest('.mask').hide();
         });
 
-        $get('.mask')
+        $('.mask')
             .css('background-color', 'rgba(0, 0, 0, 0.4)')
             .css('padding-left', 0)
             .hide()
