9e3e183916c9e4bfdbcdcbdac207a16e4f8946e0
Robbin.Zhu <robbin@59124.com>
2016-06-20 16:09:21 +0800

    WIP: portal page

--- index.js
+++ index.js
@@ -1,31 +1,6 @@
 "use strict";
 var fs = require('fs');
 var pages = require('./util/page');
-//for not dev environment, cache all the compiled templates
-var useCache = ('dev' !== (process.env.NODE_ENV || 'dev'));
-var tplCache = {};
-
-function compileTpl(jade, file) {
-    var content = fs.readFileSync(file, 'utf-8');
-    return jade.compile(content, {
-        basedir: __dirname,
-        filename: file
-    });
-}
-
-function getTplFn(jade, file) {
-    var fn;
-    if (useCache) {
-        fn = tplCache[file];
-        if (!fn) {
-            fn = compileTpl(jade, file);
-            tplCache[file] = fn;
-        }
-    } else {
-        fn = compileTpl(jade, file);
-    }
-    return fn;
-}
 
 module.exports = {
     render: function(lang, page, jade, req, res, next) {
@@ -37,8 +12,7 @@ module.exports = {
         if (!page_info) {
             return next();
         }
-        var fn = getTplFn(jade, __dirname + '/views/portal/' + lang + '/' + page + '.jade');
-        res.send(fn({
+        res.render(__dirname + '/views/portal/' + lang + '/' + page + '.jade', {
             page: page,
             lang: lang,
             title: page_info.title,
