254df2195c51ae463a48571279b6679b15159e79
田杰 <jefftian@hcdlearning.com>
2016-06-27 15:46:41 +0800

    Add slide animation addition to refresh.

--- public/js/pjax.js
+++ public/js/pjax.js
@@ -27,10 +27,25 @@ $(function () {
         }
     }
 
+    var slidePages = ['/portal/zh/about-us', '/portal/zh/team', '/portal/zh/video', '/portal/zh/contact-us', '/portal/zh/join-us'];
+
+    function getAnimation(current, next) {
+        var currentIndex = slidePages.indexOf(current);
+        var nextIndex = slidePages.indexOf(next);
+
+        if (currentIndex === nextIndex) {
+            return refresh;
+        }
+
+        return currentIndex < nextIndex ? slideLeftToRight : slideRightToLeft;
+
+    }
+
     document.addEventListener('click', function (e) {
         if (e.target.nodeName !== 'A') {
             return true;
         }
+
         var href = e.target.href ? e.target.href.toLowerCase() : '';
         if (e.target.getAttribute('use-pjax') === 'false' || !href.length ||
             (href.charAt(0) == '#') ||
@@ -39,10 +54,13 @@ $(function () {
         ) {
             return true;
         }
+
         e.preventDefault();
         e.stopPropagation();
-        load(href, e.target, true);
+
+        load(href, e.target, true, getAnimation(location.pathname, e.target.pathname));
     }, false);
+
     window.addEventListener("popstate", function () {
         if (bodyCache && bodyCache.length) {
             refresh(bodyCache.pop(), titleCache.pop(), bodyDataCache.pop());
@@ -56,27 +74,130 @@ $(function () {
     });
 
     function refresh(bodyHTML, titleHTML, data, target) {
-        $(document).trigger('pjax/refresh');
+        animateWith(titleHTML, data, target, function () {
+            body.classList.add('hide');
+
+            setTimeout(function () {
+                body.innerHTML = bodyHTML;
+                body.classList.remove('hide');
+            }, 100);
+        });
+    }
+
+    function ensureBodyAndTitle() {
         body = body || document.querySelector('.body');
         title = title || document.querySelector('title');
+    }
 
-        // var prevBody = document.createElement('div');
-        // prevBody.innerHTML = body.innerHTML;
-        // $(prevBody).css({
-        //     position: 'absolute',
-        //     left: '-90%',
-        //     right: '90%'
-        // });
-        // $(prevBody).insertBefore($(body));
+    function animateWith(titleHTML, data, target, animation) {
+        $(document).trigger('pjax/refresh');
+        ensureBodyAndTitle();
 
-        body.innerHTML = bodyHTML;
+        animation();
+
+        setData(data, titleHTML);
+        $(document).trigger('pjax/done');
+    }
+
+    function cloneOldSlide(then) {
+        var prevBody = document.createElement('div');
+        prevBody.innerHTML = body.innerHTML;
+
+        var $prevBody = $(prevBody);
+        var $body = $(body);
+
+        $prevBody.css({
+            position: 'absolute',
+            left: '0',
+            right: '0'
+        });
+
+        $prevBody.insertBefore($body);
+
+        then && then($prevBody, $body);
+
+        return {$prevBody: $prevBody, $body: $body};
+    }
+
+    function placeToRight($next) {
+        $next.css({
+            position: 'absolute',
+            left: '100%',
+            right: '-100%'
+        });
+    }
+
+    function moveToLeft($prev) {
+        $prev.animate({
+            left: '-100%',
+            right: '100%'
+        }, 'slow', function () {
+            $prev.remove();
+        });
+    }
+
+    function slideLeftToRight(bodyHTML, titleHTML, data, target) {
+        animateWith(titleHTML, data, target, function () {
+            cloneOldSlide(function ($prev, $next) {
+                body.innerHTML = bodyHTML;
+
+                placeToRight($next);
+
+                moveToLeft($prev);
+
+                returnToOriginalPos($next);
+            });
+        });
+    }
+
+
+    function setData(data, titleHTML) {
         Object.keys(data).forEach(function (key) {
             body.setAttribute('data-' + key, data[key]);
         });
         if (title) {
             title.innerHTML = titleHTML;
         }
-        $(document).trigger('pjax/done');
+    }
+
+    function returnToOriginalPos($next) {
+        $next.animate({
+            left: '0',
+            right: '0'
+        }, 'slow', function () {
+            $next.css('position', 'relative');
+        });
+    }
+
+    function placeToLeft($next) {
+        $next.css({
+            position: 'absolute',
+            left: '-100%',
+            right: '100%'
+        });
+    }
+
+    function moveToRight($prev) {
+        $prev.animate({
+            left: '100%',
+            right: '-100%'
+        }, 'slow', function () {
+            $prev.remove();
+        });
+    }
+
+    function slideRightToLeft(bodyHTML, titleHTML, data, target) {
+        animateWith(titleHTML, data, target, function () {
+            cloneOldSlide(function ($prev, $next) {
+                body.innerHTML = bodyHTML;
+
+                placeToLeft($next);
+
+                moveToRight($prev);
+
+                returnToOriginalPos($next);
+            });
+        });
     }
 
     function getData(attributes) {
@@ -90,7 +211,7 @@ $(function () {
         return data;
     }
 
-    function load(url, target, push) {
+    function load(url, target, push, animation) {
         $(document).trigger('pjax/start');
         $.get(url, function (html) {
             body = body || document.querySelector('.body');
@@ -106,7 +227,9 @@ $(function () {
             }
             var bodyElement = div.querySelector('.body');
             var titleElement = div.querySelector('title');
-            refresh(bodyElement.innerHTML, titleElement && titleElement.innerHTML, getData(bodyElement.attributes), target);
+
+            animation = animation || refresh;
+            animation(bodyElement.innerHTML, titleElement && titleElement.innerHTML, getData(bodyElement.attributes), target);
         });
     }
 });
