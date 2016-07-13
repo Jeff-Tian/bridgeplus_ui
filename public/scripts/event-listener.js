24c292b007a97cfe701d8dc60171981ba147a14d
Robbin.Zhu <robbin@59124.com>
2016-07-13 15:40:10 +0800

    update modal close function

--- public/scripts/event-listener.js
+++ public/scripts/event-listener.js
@@ -12,6 +12,13 @@
             });
     }
 
+    function hideModal($modal) {
+        $modal.removeClass('show');
+        var $video = $modal.find('video');
+        if ($video.length) {
+            $video[0].pause();
+        }
+    }
     $(document)
         .on('show_content', function(event, source, svgGroup) {
             controlShowHideOn('.group-description', svgGroup);
@@ -19,11 +26,17 @@
             controlShowHideOn('.services', svgGroup);
         })
         .on('click', '.mask .close.button', function() {
-            var $this = $(this).closest('.mask').removeClass('show');
-
-            var $video = $this.find('video');
-            if ($video.length) {
-                $video[0].pause();
+            hideModal($(this).closest('.mask'));
+        })
+        .on('click', '.mask .shadow-box', function(e) {
+            e.stopPropagation();
+        })
+        .on('click', '.mask', function() {
+            hideModal($(this));
+        })
+        .on('keydown', function(e) {
+            if (e.keyCode === 27) {
+                hideModal($('.mask.show'));
             }
         })
         .on('click', '[data-modal-selector]', function($event) {
