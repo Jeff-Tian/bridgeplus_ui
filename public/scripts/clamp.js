355ec089573ff6c6376db329762862b247387dfe
田杰 <jefftian@hcdlearning.com>
2016-07-21 17:35:29 +0800

    Optimization for small screen.

---
+++ public/scripts/clamp.js
@@ -0,0 +1,13 @@
+$(function () {
+    function clamp() {
+        var clamps = document.getElementsByClassName('clamp');
+        for (var i = 0; i < clamps.length; i++) {
+            var e = clamps[i];
+            $clamp(e, {clamp: 5});
+        }
+    }
+
+    clamp();
+
+    $(document).on('pjax/done', clamp);
+});
