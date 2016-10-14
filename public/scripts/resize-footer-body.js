c82af886538ef07959154f78f68596a6dfa79fed
田杰 <jeff.tian@outlook.com>
2016-10-14 10:55:06 +0800

    Delete console.

--- public/scripts/resize-footer-body.js
+++ public/scripts/resize-footer-body.js
@@ -18,35 +18,26 @@ $(function () {
         var $footerBody = $('.body'),
             $body = $('body');
 
-        console.log('resizing footer body');
         var totalHeight = $body.height();
-        console.log('total = ', totalHeight);
         var topHeight = $('.to-top').outerHeight();
-        console.log('top = ', topHeight);
         var footerHeight = $('#footer').outerHeight();
-        console.log('footer = ', footerHeight);
         var bodyHeight = totalHeight - topHeight - footerHeight;
-        console.log('body height = ', bodyHeight);
         $footerBody.height(bodyHeight);
     }
 
     $(window).on('resize', function () {
-        console.log('action when resize');
         resizeFooterBody();
         setTimeout(prepareCarousel, 500);
     });
 
     $(document).on('pjax/done', function () {
-        console.log('action when pjax/done');
         setTimeout(prepareCarousel, 500);
     }).on('show_content', function () {
-        console.log('action when show_content');
         resizeFooterBody();
         setTimeout(prepareCarousel, 500);
     });
 
     $('.to-top, #footer').find('img').each(function (index, element) {
-        console.log('img load, changed height');
         $(element).on('load', resizeFooterBody);
     });
 });
